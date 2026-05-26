fecde18546854049c116f87b4c020b4b97395821
antbeltran <anthonybltrn@gmail.com>
2026-05-26 10:54:32 -0500

    created garnish enum

--- src/main/java/com/pluralsight/drink/Garnish.java
+++ src/main/java/com/pluralsight/drink/Garnish.java
@@ -1,4 +1,26 @@
 package com.pluralsight.drink;
 
 public enum Garnish {
+    LIME("Lime",0.00),
+    LEMON("Lemon", 0.00),
+    STRAWBERRIES("Strawberries", 0.50),
+    CHERRIES("Cherries", 0.50),
+    ORANGE("Oranges", 0.50),
+    CINNAMON_STICK("Cinnamon Stick", 0.50);
+
+    private final String name;
+    private final double price;
+
+    Garnish(String name,double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
