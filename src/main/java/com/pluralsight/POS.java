73ff8972f8acfa9bb55952e79dc2314056c88ed9
antbeltran <anthonybltrn@gmail.com>
2026-05-29 08:29:33 -0500

    mvp

--- src/main/java/com/pluralsight/POS.java
+++ src/main/java/com/pluralsight/POS.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class POS {
     public static void main(String[] args) throws IOException {
+        //instantiating a new menu
         IMenu menu = new IMenu();
         //kicking off the "app"
         menu.menu();
